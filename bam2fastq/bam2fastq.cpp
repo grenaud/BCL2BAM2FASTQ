da8cdb50804d9a2c78bcadabd708bc54164eff58
grenaud <gabriel.reno@gmail.com>
2014-03-09 18:23:20 +0100

    Update

--- bam2fastq/bam2fastq.cpp
+++ bam2fastq/bam2fastq.cpp
@@ -21,8 +21,22 @@
 using namespace std;
 using namespace BamTools;
 
+typedef struct { 
+
+    ogzstream single;
+    ogzstream pairr1;
+    ogzstream pairr2;
+
+    ogzstream singlef;
+    ogzstream pairr1f;
+    ogzstream pairr2f;
+
+
+ } fqwriters;
+
+
 int main (int argc, char *argv[]) {
-    // bool splitQC=false;
+    bool splitRG=false;
 
     string usage=string(""+string(argv[0])+" <options> [bam file] [output prefix]"+		     
 			"\n\nThis program will read a bam file sorted by name and produce 6 files:\n"+
@@ -33,12 +47,12 @@ int main (int argc, char *argv[]) {
 			"\n\t[output prefix]_r1.fq.fail.gz\tFor first mates for clusters that failed"+
 			"\n\t[output prefix]_r2.fq.fail.gz\tFor second mates for clusters that failed\n"
 
-			// "Options:\n"+
-			// "\t\t"+"--qc"+"\t"+"Split quality failed and non-quality failed (Default: "+booleanAsString(splitQC)+" )\n"
+			"Options:\n"+
+			"\t\t"+"--rg"+"\t"+"Split according to read group (Default: "+booleanAsString(splitRG)+" )\n"
 			);		      
 
 
-    if(argc != 3 ||
+    if(argc  < 3 ||
        (argc == 2 && (string(argv[1]) == "-h" || string(argv[1]) == "--help") )
        ){
 	cerr << "Usage "<<usage<<endl;
@@ -47,10 +61,10 @@ int main (int argc, char *argv[]) {
 
     for(int i=1;i<(argc-2);i++){ 
 
-	// if(string(argv[i]) == "--qc" ) {
-	//     splitQC=true;
-	//     continue;
-	// }
+	if(string(argv[i]) == "--rg" ) {
+	    splitRG=true;
+	    continue;
+	}
 
         cerr<<"Unknown option "<<argv[i] <<" exiting"<<endl;
         return 1;           
@@ -60,27 +74,29 @@ int main (int argc, char *argv[]) {
     string bamfile = string(argv[argc-2]);
     string outdir  = string(argv[argc-1]);
 
-    string outdirs   = outdir+".fq.gz";
-    string outdir1   = outdir+"_r1.fq.gz";
-    string outdir2   = outdir+"_r2.fq.gz";
-    string outdirsf  = outdir+".fq.fail.gz";
-    string outdir1f  = outdir+"_r1.fail.fq.gz";
-    string outdir2f  = outdir+"_r2.fail.fq.gz";
 
 
-    ogzstream single;
-    single.open(outdirs.c_str(), ios::out);
-    ogzstream pairr1;
-    pairr1.open(outdir1.c_str(), ios::out);
-    ogzstream pairr2;
-    pairr2.open(outdir2.c_str(), ios::out);
+    fqwriters onereadgroup;
+    map<string,fqwriters *> rg2fqwriters;
 
-    ogzstream singlef;
-    singlef.open(outdirsf.c_str(), ios::out);
-    ogzstream pairr1f;
-    pairr1f.open(outdir1f.c_str(), ios::out);
-    ogzstream pairr2f;
-    pairr2f.open(outdir2f.c_str(), ios::out);
+
+    if(!splitRG){
+	string outdirs   = outdir+".fq.gz";
+	string outdir1   = outdir+"_r1.fq.gz";
+	string outdir2   = outdir+"_r2.fq.gz";
+	string outdirsf  = outdir+".fq.fail.gz";
+	string outdir1f  = outdir+"_r1.fail.fq.gz";
+	string outdir2f  = outdir+"_r2.fail.fq.gz";
+
+
+	onereadgroup.single.open(outdirs.c_str(), ios::out);
+	onereadgroup.pairr1.open(outdir1.c_str(), ios::out);
+	onereadgroup.pairr2.open(outdir2.c_str(), ios::out);
+
+	onereadgroup.singlef.open(outdirsf.c_str(), ios::out);
+	onereadgroup.pairr1f.open(outdir1f.c_str(), ios::out);
+	onereadgroup.pairr2f.open(outdir2f.c_str(), ios::out);
+    }
 
     BamReader reader;
     if ( !reader.Open(bamfile) ) {
@@ -96,6 +112,39 @@ int main (int argc, char *argv[]) {
     bool al2Null=true;
     while ( reader.GetNextAlignment(al) ) {
 	totalReads++;
+	
+	string rgTag;
+
+	if(splitRG){
+ 
+	    if(al.HasTag("RG")){
+		al.GetTag("RG",rgTag);
+	    }else{
+		rgTag="unknown";
+	    }
+
+
+	    if(rg2fqwriters.find(rgTag) == rg2fqwriters.end()){ //new
+		cerr<<"Found new RG "<<rgTag<<endl;
+
+		rg2fqwriters[rgTag] = new  fqwriters();
+		string outdirs   = outdir+"rg_"+rgTag+""+".fq.gz";
+		string outdir1   = outdir+"rg_"+rgTag+""+"_r1.fq.gz";
+		string outdir2   = outdir+"rg_"+rgTag+""+"_r2.fq.gz";
+		string outdirsf  = outdir+"rg_"+rgTag+""+".fq.fail.gz";
+		string outdir1f  = outdir+"rg_"+rgTag+""+"_r1.fail.fq.gz";
+		string outdir2f  = outdir+"rg_"+rgTag+""+"_r2.fail.fq.gz";
+
+		rg2fqwriters[rgTag]->single.open(outdirs.c_str(), ios::out);
+		rg2fqwriters[rgTag]->pairr1.open(outdir1.c_str(), ios::out);
+		rg2fqwriters[rgTag]->pairr2.open(outdir2.c_str(), ios::out);
+
+		rg2fqwriters[rgTag]->singlef.open(outdirsf.c_str(), ios::out);
+		rg2fqwriters[rgTag]->pairr1f.open(outdir1f.c_str(), ios::out);
+		rg2fqwriters[rgTag]->pairr2f.open(outdir2f.c_str(), ios::out);
+	    }
+
+	}
 
 	if(al.IsPaired() ){
 	    if( al2Null ){
@@ -108,13 +157,28 @@ int main (int argc, char *argv[]) {
 		    return 1;
 		}
 
-		
-		if(!al.IsFailedQC() && !al2.IsFailedQC() ){//read passed
-		    pairr2 <<"@"<< al.Name<<"/2" <<endl <<al.QueryBases<<endl<<"+"<<endl <<al.Qualities<<endl;
-		    pairr1 <<"@"<<al2.Name<<"/1"<<endl <<al2.QueryBases<<endl<<"+"<<endl<<al2.Qualities<<endl;
+
+		if(splitRG){
+
+		    
+		    if(!al.IsFailedQC() && !al2.IsFailedQC() ){//read passed
+			rg2fqwriters[rgTag]->pairr2 <<"@"<< al.Name<<"/2" <<endl <<al.QueryBases<<endl<<"+"<<endl <<al.Qualities<<endl;
+			rg2fqwriters[rgTag]->pairr1 <<"@"<<al2.Name<<"/1"<<endl <<al2.QueryBases<<endl<<"+"<<endl<<al2.Qualities<<endl;
+		    }else{
+			rg2fqwriters[rgTag]->pairr2f<<"@"<< al.Name<<"/2" <<endl <<al.QueryBases<<endl<<"+"<<endl <<al.Qualities<<endl;
+			rg2fqwriters[rgTag]->pairr1f<<"@"<<al2.Name<<"/1"<<endl <<al2.QueryBases<<endl<<"+"<<endl<<al2.Qualities<<endl;
+		    }
+
+
 		}else{
-		    pairr2f<<"@"<< al.Name<<"/2" <<endl <<al.QueryBases<<endl<<"+"<<endl <<al.Qualities<<endl;
-		    pairr1f<<"@"<<al2.Name<<"/1"<<endl <<al2.QueryBases<<endl<<"+"<<endl<<al2.Qualities<<endl;
+		
+		    if(!al.IsFailedQC() && !al2.IsFailedQC() ){//read passed
+			onereadgroup.pairr2 <<"@"<< al.Name<<"/2" <<endl <<al.QueryBases<<endl<<"+"<<endl <<al.Qualities<<endl;
+			onereadgroup.pairr1 <<"@"<<al2.Name<<"/1"<<endl <<al2.QueryBases<<endl<<"+"<<endl<<al2.Qualities<<endl;
+		    }else{
+			onereadgroup.pairr2f<<"@"<< al.Name<<"/2" <<endl <<al.QueryBases<<endl<<"+"<<endl <<al.Qualities<<endl;
+			onereadgroup.pairr1f<<"@"<<al2.Name<<"/1"<<endl <<al2.QueryBases<<endl<<"+"<<endl<<al2.Qualities<<endl;
+		    }
 		}
 		pairedreads++;
 	    }
@@ -122,28 +186,66 @@ int main (int argc, char *argv[]) {
 	}else{
 	    if( al2Null ){
 
+		if(splitRG){
 
-		if(!al.IsFailedQC() ){//read passed
-		    single <<"@"<<al.Name<<endl<<al.QueryBases<<endl<<"+"<<endl<<al.Qualities<<endl;
+
 		}else{
-		    singlef<<"@"<<al.Name<<endl<<al.QueryBases<<endl<<"+"<<endl<<al.Qualities<<endl;
-		}
 
+
+		    if(splitRG){
+
+			
+			if(!al.IsFailedQC() ){//read passed
+			    rg2fqwriters[rgTag]->single <<"@"<<al.Name<<endl<<al.QueryBases<<endl<<"+"<<endl<<al.Qualities<<endl;
+			}else{
+			    rg2fqwriters[rgTag]->singlef<<"@"<<al.Name<<endl<<al.QueryBases<<endl<<"+"<<endl<<al.Qualities<<endl;
+			}
+
+		    }else{
+
+			if(!al.IsFailedQC() ){//read passed
+			    onereadgroup.single <<"@"<<al.Name<<endl<<al.QueryBases<<endl<<"+"<<endl<<al.Qualities<<endl;
+			}else{
+			    onereadgroup.singlef<<"@"<<al.Name<<endl<<al.QueryBases<<endl<<"+"<<endl<<al.Qualities<<endl;
+			}
+
+		    }
+		}
 		singlereads++;
 	    }else{
 		cerr << "Seq#1 " <<al.Name <<" was found next to a lone paired reads " <<al2.Name <<" exiting " << endl;
 		return 1;
 	    }
+
 	}
 	al2Null=true;
     }
 
-    single.close();
-    pairr1.close();
-    pairr2.close();
-    singlef.close();
-    pairr1f.close();
-    pairr2f.close();
+    if(!splitRG){
+	
+	onereadgroup.single.close();
+	onereadgroup.pairr1.close();
+	onereadgroup.pairr2.close();
+	onereadgroup.singlef.close();
+	onereadgroup.pairr1f.close();
+	onereadgroup.pairr2f.close();
+	
+    }else{
+
+	vector<string> vsKey = allKeysMap(rg2fqwriters);
+	
+	for(unsigned int i=0;i<vsKey.size();i++){
+
+	    rg2fqwriters[ vsKey[i] ]->single.close();
+	    rg2fqwriters[ vsKey[i] ]->pairr1.close();
+	    rg2fqwriters[ vsKey[i] ]->pairr2.close();
+	    rg2fqwriters[ vsKey[i] ]->singlef.close();
+	    rg2fqwriters[ vsKey[i] ]->pairr1f.close();
+	    rg2fqwriters[ vsKey[i] ]->pairr2f.close();
+
+	}
+	
+    }
 	
     cerr<<"Program finished successfully, wrote "<<totalReads<<" reads, "<<singlereads<<" single reads and "<<pairedreads<<" pairs"<<endl;
     return 0;
